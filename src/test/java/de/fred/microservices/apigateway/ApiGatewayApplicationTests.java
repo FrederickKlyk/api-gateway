b882cc518f233704aa688b0e4b21a3282b835825
Fred <frederick.klyk@gmail.com>
2024-02-24 17:35:22 +0100

    init commit

---
+++ src/test/java/de/fred/microservices/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package de.fred.microservices.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
